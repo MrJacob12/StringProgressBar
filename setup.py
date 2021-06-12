345c85f3690b47a480966f398881cecc86069715
MrJacob <anubis122003@gmail.com>
2021-06-12 18:05:42 +0200

    1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
   name='StringProgressBar',
-  version='1.0.0',
+  version='1.0.1',
   description='Simple string progress bar made for discord bots. Its usable everywhere.',
   long_description= open('README.md', encoding="utf8").read() + '\n\n' + open('CHANGELOG.txt', encoding="utf8").read(),
   long_description_content_type='text/markdown',
@@ -19,7 +19,7 @@ setup(
   author_email='jacob@fern.fun',
   license='MIT', 
   classifiers=classifiers,
-  keywords='email', 
+  keywords='String Progress Bar', 
   packages=find_packages(),
   install_requires=[''] 
 )
